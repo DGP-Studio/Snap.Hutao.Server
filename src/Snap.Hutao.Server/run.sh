c0bb57ec6054a22374733631260866672577e429
Platelets <wenkanedward@outlook.com>
2022-10-14 12:43:25 +0800

    change position of dockerfile and change connectionstring:

---
+++ src/Snap.Hutao.Server/run.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+
+
+docker build -t snapserver .
+
+docker run -p 8888:80 -e "ConnectionStrings:Snap_DB"="server=172.17.0.1;port=3306;user=root;password=root; database=iwut_news;" snapserver snapserver
