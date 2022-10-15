1916721dc4b6582676b2c54950db992478192aac
Masterain <i@irain.in>
2022-10-15 01:36:52 -0700

    Update run.sh

--- src/Snap.Hutao.Server/run.sh
+++ src/Snap.Hutao.Server/run.sh
@@ -16,5 +16,4 @@ docker build -t $imageName:$version -f Dockerfile .
 echo port is $port
 docker run -d -p $port:80 \
     --name="$containerName-$version" \
-    -e "ConnectionStrings:Snap_DB"="server=172.17.0.1;port=3306;user=root;password=root;database=xxxxxx;" \
     $imageName:$version 
