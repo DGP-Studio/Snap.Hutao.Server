4cf12230571dd1150814ad676a2058c86694ed2d
DismissedLight <1686188646@qq.com>
2025-03-06 14:23:39 +0800

    SendDefaultPii

--- src/Snap.Hutao.Server/Snap.Hutao.Server/Program.cs
+++ src/Snap.Hutao.Server/Snap.Hutao.Server/Program.cs
@@ -48,6 +48,7 @@ public static class Program
             options.Release = $"{DateTimeOffset.Now:yyyy.M.d.hm}";
             options.Dsn = "http://7de19654a539bfdd56a798ce89e85137@host.docker.internal:9510/7";
             options.TracesSampleRate = 1D;
+            options.SendDefaultPii = true;
         });
 
         appBuilder.Services.AddSentryTunneling();
