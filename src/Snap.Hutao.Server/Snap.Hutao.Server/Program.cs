ac7733677f22460224d6b1c23162dd31f118c2b9
qhy040404 <qhy040404@163.com>
2025-04-02 11:09:25 +0800

    add static resource endpoint

--- src/Snap.Hutao.Server/Snap.Hutao.Server/Program.cs
+++ src/Snap.Hutao.Server/Snap.Hutao.Server/Program.cs
@@ -33,6 +33,7 @@ using Snap.Hutao.Server.Service.ReCaptcha;
 using Snap.Hutao.Server.Service.Redeem;
 using Snap.Hutao.Server.Service.RoleCombat;
 using Snap.Hutao.Server.Service.Sentry;
+using Snap.Hutao.Server.Service.StaticResource;
 using Snap.Hutao.Server.Service.Telemetry;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
@@ -115,6 +116,7 @@ public static class Program
             .AddScoped<PizzaHelperRecordService>()
             .AddScoped<RecordService>()
             .AddScoped<SpiralAbyssStatisticsService>()
+            .AddScoped<StaticResourceService>()
             .AddScoped<TelemetryService>()
             .AddScoped<RedeemService>()
             .AddSingleton<AfdianWebhookService>()
---
+++ src/Snap.Hutao.Server/Snap.Hutao.Server/Service/StaticResource/ImageToken.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) DGP Studio. All rights reserved.
+// Licensed under the MIT license.
+
+namespace Snap.Hutao.Server.Service.StaticResource;
+
+public sealed class ImageToken
+{
+    [JsonPropertyName("username")]
+    public string Username { get; set; } = default!;
+
+    [JsonPropertyName("token")]
+    public string Token { get; set; } = default!;
+
+    [JsonPropertyName("expire")]
+    public long Expire { get; set; }
+}
